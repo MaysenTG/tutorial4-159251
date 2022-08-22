ae858fa5f2bc4693ae0e66575bb53e5a5985da58
Maysen Greenwood <greenwood.maysen@gmail.com>
2022-08-22 23:10:02 +1200

    Initialize project with Calc class and jUnit tests

---
+++ src/test/java/jar/TestCalc.java
@@ -0,0 +1,40 @@
+package jar;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestCalc 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TestCalc( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TestCalc.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+      Calc c = new Calc();
+      
+      assertEquals(c.add(1,2), 3);
+    }
+}
